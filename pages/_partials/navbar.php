78c612d0e55091b2f24d1ab95fb4a7d863cad627
Muhammad Khoiri Muzakki <muhammadkhoirimuzaki@gmail.com>
2020-12-07 14:33:56 +0700

    update h-1

--- pages/_partials/navbar.php
+++ pages/_partials/navbar.php
@@ -11,7 +11,7 @@
       <a class="navbar-brand" style="color : white" href="../dasbor"> Warga Daring</a>
     </div>
     <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav navbar-right">
+      <ul class="nav navbar-nav" style="float: right;">
         <p class="navbar-text"><i class="fa fa-user"></i> Hai, <?php echo $_SESSION['user']['nama_user'] ?></p>
         <li class="nav-item" >
           <a href="../dasbor" class="nav-link" style="color : white;" onMouseOver="this.style.color='blue'" onMouseOut="this.style.color='white'"><i class="fa fa-home"></i> Dashbord</a>
